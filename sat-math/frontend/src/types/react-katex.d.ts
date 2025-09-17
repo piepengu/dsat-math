451b3abca68703c7a92078f37cb696d6a16b673f
piepengu <135545742+piepengu@users.noreply.github.com>
2025-09-17 00:45:49 -0500

    fix(frontend): add types for react-katex

---
+++ sat-math/frontend/src/types/react-katex.d.ts
@@ -0,0 +1,10 @@
+﻿declare module 'react-katex' {
+  import * as React from 'react';
+  export interface KatexProps {
+    math: string;
+    errorColor?: string;
+    renderError?: (error: any) => React.ReactNode;
+  }
+  export const BlockMath: React.FC<KatexProps>;
+  export const InlineMath: React.FC<KatexProps>;
+}
